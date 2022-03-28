a7fc8891534c7837bffea2d6706c08b5f54f17e8
Recep Selim Ağırman <16990318+selimrecep@users.noreply.github.com>
2022-03-28 23:51:00 +0300

    Updated main and readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Battleships
   Just a small project wanted to create to practice C++
+
+# TODO List
+  [ ] Fix ConsoleBuffer class
+  [ ] Refactor base code
+  [ ] Make use of heap with smart pointers
--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include "Game/Game.h"
+#include "Game/Board/GameCell.h"
+#include "Game/Board/Point.h"
+#include "Gui/GuiManager.h"
 
 int main() {
   Game game{};
+  GuiManager gManager{ game };
+
+  gManager.welcomeText();
+  gManager.askForNames();
+
+  int a;
+  std::cin >> a;
 }
